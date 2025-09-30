5bd3b4ccea18726d88a5674ef38578ba1b1bb902
Alexis de la Cruz Hernández <alexisdelacruzhernandez@gmail.com>
2025-09-29 21:14:15 -0300

    Segunda consigna de TP5

--- 3_trabajos_practicos/tp5/kernel_module.c
+++ 3_trabajos_practicos/tp5/kernel_module.c
@@ -8,20 +8,28 @@
 // Etiqueta para el autor del modulo
 #define AUTHOR	"Alexis Hernandez"
 
+#define LED_PIN 21
+
 // Puntero para primer hilo
-static struct task_struct *thread1_hola;
+static struct task_struct *thread1_on;
 // Puntero para segundo hilo
-static struct task_struct *thread2_chau;
+static struct task_struct *thread2_off;
+static uint8_t led_pin = LED_PIN;
+
+
 
 /**
  * @brief Envia el mensaje "Hola desde el kernel!" cada 500ms
  */
-static int thread1_hola_f(void *params) {
+static int thread1_on_f(void *params) {
+	uint8_t led = *(uint8_t*) params;
+	msleep(500);
 	while(!kthread_should_stop()) {
+		gpio_set(led);
 		// Mensaje cuando corre la tarea
-		printk(KERN_INFO "%s: Hola desde el kernel!\n", AUTHOR);
+		printk(KERN_INFO "%s: ON\n", AUTHOR);
 		// Demora de medio segundo
-		msleep(500);
+		msleep(1000);
 	}
 	return 0;
 }
@@ -29,12 +37,14 @@ static int thread1_hola_f(void *params) {
 /**
  * @brief Envia el mensaje "Chau desde el kernel!" cada 500ms
  */
-static int thread2_chau_f(void *params) {
+static int thread2_off_f(void *params) {
+	uint8_t led = *(uint8_t*) params;
 	while(!kthread_should_stop()) {
+		gpio_clr(led);
 		// Mensaje cuando corre la tarea
-		printk(KERN_INFO "%s: Chau desde el kernel!\n", AUTHOR);
+		printk(KERN_INFO "%s: OFF\n", AUTHOR);
 		// Demora de medio segundo
-		msleep(500);
+		msleep(1000);
 	}
 	return 0;
 }
@@ -43,30 +53,36 @@ static int thread2_chau_f(void *params) {
  * @brief Se llama cuando el modulo se carga en el kernel
 */
 static int __init kernel_module_init(void) {
+
+	if(gpio_map() == NULL) {
+		printk(KERN_ERR "%s: Error al solicitar memoria virtual\n", AUTHOR);
+		return -1;
+	}
+	gpio_set_dir_output(led_pin);
 	// Mensaje para el kernel
 	printk(KERN_INFO "%s: Insertando el modulo de kernel\n", AUTHOR);
 	// Creacion de tarea 1
-	thread1_hola = kthread_run(
-		thread1_hola_f,
-		NULL,
-		"thread1_hola"
+	thread1_on = kthread_run(
+		thread1_on_f,
+		(void*) &led_pin,
+		"thread1_on"
 	);
 	// Verificacion de error
-	if(IS_ERR(thread1_hola)) {
-		printk(KERN_ERR "%s: Error al crear el hilo thread1_hola\n", AUTHOR);
+	if(IS_ERR(thread1_on)) {
+		printk(KERN_ERR "%s: Error al crear el hilo thread1_on\n", AUTHOR);
 		return -1;
 	}
 	// Creacion de tarea 2
-	thread2_chau = kthread_run(
-		thread2_chau_f,
-		NULL,
-		"thread2_chau"
+	thread2_off = kthread_run(
+		thread2_off_f,
+		(void*) &led_pin,
+		"thread2_off"
 	);
 	// Verificacion de error
-	if(IS_ERR(thread2_chau)) {
-		printk(KERN_ERR "%s: Error al crear el hilo thread2_chau\n", AUTHOR);
+	if(IS_ERR(thread2_off)) {
+		printk(KERN_ERR "%s: Error al crear el hilo thread2_off\n", AUTHOR);
 		// Eliminamos la tarea anterior
-		kthread_stop(thread1_hola);
+		kthread_stop(thread1_on);
 		return -1;
 	}
 	return 0;
@@ -79,11 +95,11 @@ static void __exit kernel_module_exit(void) {
 	// Mensaje
 	pr_info("%s: Removiendo el modulo del kernel\n", AUTHOR);
 	// Eliminamos los hilos creados
-	if(thread1_hola) {
-		kthread_stop(thread1_hola);
+	if(thread1_on) {
+		kthread_stop(thread1_on);
 	}
-	if(thread2_chau) {
-		kthread_stop(thread2_chau);
+	if(thread2_off) {
+		kthread_stop(thread2_off);
 	}
 }
 
@@ -94,4 +110,4 @@ module_exit(kernel_module_exit);
 // Informacion del modulo
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR(AUTHOR);
-MODULE_DESCRIPTION("Trabajo practico 5 v1");
+MODULE_DESCRIPTION("Trabajo practico 5 v2");
